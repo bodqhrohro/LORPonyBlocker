dffe2d22386d65e2b9fad9b08b64481427b70d7e
bodqhrohro <bodqhrohro@yandex.ru>
2013-09-13 23:53:18 +0300

    version 0.2.0

--- LORPonyBlocker.user.js
+++ LORPonyBlocker.user.js
@@ -10,11 +10,20 @@
 
  var ponyfaglist=[
  'a1batross',
+ 'aidan',
+ 'Akamanah',
  'amomymous',
  'AnimusPEXUS',
+ 'Anonymous',
  'Antimatter',
+ 'AptGet',
+ 'atrus',
  'avertex',
+ 'Bagrov',
+ 'bsdfun',
+ 'Chaser_Andrey',
  'Cooler',
+ 'com',
  'Copycat',
  'cryptohedge',
  'Dark_SavanT',
@@ -22,44 +31,174 @@
  'dearboy',
  'Deneb',
  'derlafff',
+ 'devpony',
+ 'Dispetcher14',
+ 'DoctorSinus',
  'drBatty',
  'druganddrop-2',
  'ekzotech',
+ 'emulek',
  'essir',
  'evilmanul',
  'Extraterrestrial',
  'Falcon-peregrinus',
+ 'Flyn',
  'fornlr',
+ 'HiddenComplexity',
  'hizel',
  'Hoodoo',
+ 'IceAlchemist',
  'Igorrr',
+ 'Ingvarr',
  'IPR',
+ 'ishido',
  'ishitori',
+ 'kasthack',
  'kiverattes',
  'ktk',
+ 'ktulhu666',
+ 'Landgraf',
  'Legioner',
+ 'Le_Raux',
+ 'Lonli-Lockley',
  'Lorchanin',
+ 'melkor217',
  'mephistopheles',
  'mopsene',
  'morse',
+ 'NetForHack',
+ 'Newlifer',
  'Old_Hamster',
+ 'OldWiseCat',
  'olibjerd',
  'PaxthonFettel',
+ 'pevzi',
+ 'procoder99',
  'proud_anon',
+ 'quowah',
+ 'reliktt',
  'rikardoac',
  'Romaboy',
  'rtvd',
- 'sudoer',
- 'tailgunner',
+ 'StalinEXE',
+ 'thelonelyisland',
+ 'Thero',
+ 'uin',
  'vazgen05',
- 'x0r'
+ 'veneperkele',
+ 'voronin',
+ 'x0r',
+ 'Yaourt',
+ 'yaspol'
  ];
- var checkNick=function(){
+ 
+ function readSetting(name) {
+  return localStorage.getItem(name) == 'true';
+ }
+ 
+ function toggleSetting(name) {
+  var result = readSetting(name);
+  result = !result;
+  localStorage.setItem(name, result ? 'true' : 'false');
+  return result;
+ }
+ 
+ function displaySetting(name, defaultChecked) {
+  var result = readSetting(name);
+  result = (result && !defaultChecked) || (!result && defaultChecked);
+  return result;
+ }
+
+ var settingsTab={
+  'fill_ponies': 'Заменять аватарки не-понифагов поняшами',
+  'hide_ponies': 'Скрывать аватарки понифагов'
+ };
+ 
+ var poniAvs=[
+  [
+   'applejack',
+   80
+  ],
+  [
+   'fluttershy',
+   72
+  ],
+  [
+   'pinkie_pie',
+   127
+  ],
+  [
+   'rainbow_dash',
+   176
+  ],
+  [
+   'rarity',
+   117
+  ],
+  [
+   'twilight_sparkle',
+   117
+  ]
+ ];
+ 
+ function pad3(number) {
+  var s='';
+  s+=number/100|0;
+  s+=(number/10)%10|0;
+  s+=number%10;
+  return s;
+ }
+ 
+ function randPony(){
+  var name=poniAvs[(Math.random()*6)|0];
+  var up=name[0].indexOf('_'),s_name;
+  if (up>-1){
+   s_name=name[0].substring(0,up)+name[0].substring(up+1);
+  } else {
+   s_name=name[0];
+  }
+  return 'http://www.bronyland.com/goodies/avatars/'+s_name+'/100/'+name[0]+'_avatar_'+pad3((Math.random()*name[1])|0+1)+'.png';
+ }
+ 
+ if (/settings/.test(window.location.href)){
+  var baseHr=(($('#profileForm hr:eq(0)').parent()).parent())
+  baseHr.after(
+   $('<tr><td colspan=2><hr/></td></tr>')
+  );
+  for (settings in settingsTab){
+   (function(s,st){
+    baseHr.after(
+     $('<tr/>')
+      .append(
+       $('<td/>').text(st[s])
+      )
+      .append(
+       $('<td/>').append(
+        $('<input/>')
+        .attr('type','checkbox')
+        .click(function(){
+         toggleSetting(s)
+        })
+        .prop('checked',displaySetting(s))
+       )
+      )
+    )
+   })(settings,settingsTab);
+  }
+ };
+
+var checkNick=function(){
   var nick=$(this).text();
   for (var i=0;i<ponyfaglist.length;i++){
    if (nick==ponyfaglist[i]){
-    (($(this).closest('.msg-container')).find('.userpic')).hide();
+    if (readSetting('hide_ponies')){
+     (($(this).closest('.msg-container')).find('.userpic')).hide();
+    }
+    return;
    }
+  }
+  if (readSetting('fill_ponies')){
+   (($(this).closest('.msg-container')).find('.userpic img')).attr('src',randPony()).width(100).height(100);
   }
  };
  $('article.msg a[itemprop=\'creator\']').each(checkNick);
